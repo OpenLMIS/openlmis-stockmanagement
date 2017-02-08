802f4feca46076203b5d388e2a8c292f3208a882
pfcui <cuipengfei2008@gmail.com>
2017-02-08 17:55:29 +0800

    OLMIS-1632 add test to make sure event will not be saved if saving stock cards or line items goes wrong

--- src/main/java/org/openlmis/stockmanagement/web/StockEventsController.java
+++ src/main/java/org/openlmis/stockmanagement/web/StockEventsController.java
@@ -5,7 +5,6 @@ import org.openlmis.stockmanagement.service.StockEventProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
---
+++ src/test/java/org/openlmis/stockmanagement/service/StockEventProcessorTransactionTest.java
@@ -0,0 +1,64 @@
+package org.openlmis.stockmanagement.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.openlmis.stockmanagement.BaseTest;
+import org.openlmis.stockmanagement.dto.StockEventDto;
+import org.openlmis.stockmanagement.dto.UserDto;
+import org.openlmis.stockmanagement.repository.StockEventsRepository;
+import org.openlmis.stockmanagement.testutils.StockEventDtoBuilder;
+import org.openlmis.stockmanagement.util.AuthenticationHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableWithSize.iterableWithSize;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StockEventProcessorTransactionTest extends BaseTest {
+
+  @Autowired
+  private StockEventProcessor stockEventProcessor;
+
+  @Autowired
+  private StockEventsRepository stockEventsRepository;
+
+  @MockBean
+  private AuthenticationHelper authenticationHelper;
+
+  @MockBean
+  StockCardService stockCardService;
+
+  @Test
+  public void should_not_save_event_when_save_stock_card_failed() throws Exception {
+    //given
+    UserDto user = new UserDto();
+    user.setId(UUID.randomUUID());
+    when(authenticationHelper.getCurrentUser()).thenReturn(user);
+
+    //pretend that after event is saved, saving cards and line items will go wrong
+    StockEventDto stockEventDto = StockEventDtoBuilder.createStockEventDto();
+    Mockito.doThrow(new InstantiationException()).when(stockCardService)
+            .saveFromEvent(any(StockEventDto.class), any(UUID.class), any(UUID.class));
+
+    //when
+    try {
+      stockEventProcessor.process(stockEventDto);
+    } catch (InstantiationException ex) {
+      //then: the saved event should be rolled back
+      assertThat(stockEventsRepository.findAll(), iterableWithSize(0));
+      return;
+    }
+
+    Assert.fail();
+  }
+}
