547e8a44de0bfe6159ea99366ea4cdb99d827c85
pfcui <cuipengfei2008@gmail.com>
2017-02-16 16:28:45 +0800

    add get stock card ids end point, only for showcase convenience, it will be removed later

--- src/main/java/org/openlmis/stockmanagement/web/StockCardsController.java
+++ src/main/java/org/openlmis/stockmanagement/web/StockCardsController.java
@@ -1,6 +1,7 @@
 package org.openlmis.stockmanagement.web;
 
 import org.openlmis.stockmanagement.dto.StockCardDto;
+import org.openlmis.stockmanagement.repository.StockCardRepository;
 import org.openlmis.stockmanagement.service.StockCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import static java.util.stream.StreamSupport.stream;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -19,6 +22,9 @@ public class StockCardsController {
 
   @Autowired
   private StockCardService stockCardService;
+
+  @Autowired
+  private StockCardRepository stockCardRepository;
 
   /**
    * Get stock card by id.
@@ -35,4 +41,18 @@ public class StockCardsController {
       return new ResponseEntity<>(stockCardDto, OK);
     }
   }
+
+  /**
+   * Get stock card ids.
+   *
+   * @return all stock card ids.
+   */
+  @RequestMapping(value = "/stockCardIds")
+  public ResponseEntity<String> getStockCardIds() {
+    String ids = String.join(",", stream(stockCardRepository.findAll().spliterator(), false)
+            .map(card -> card.getId().toString()).collect(Collectors.toList()));
+    String warning = "this is for showcase convenience, will be removed afterwards.";
+    return new ResponseEntity<String>(warning + ids, OK);
+  }
+
 }
