d10195f54aa34cc8507977a233ae4e385fead14c
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-12 10:52:43 +0200

    OLMIS-2611: removed unused imports

--- src/main/java/org/openlmis/stockmanagement/Application.java
+++ src/main/java/org/openlmis/stockmanagement/Application.java
@@ -31,9 +31,6 @@ import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 import java.util.Locale;
 
-import static org.apache.commons.lang.StringUtils.isBlank;
-import static org.apache.commons.lang3.LocaleUtils.toLocale;
-
 @SpringBootApplication
 @ImportResource("applicationContext.xml")
 public class Application {
