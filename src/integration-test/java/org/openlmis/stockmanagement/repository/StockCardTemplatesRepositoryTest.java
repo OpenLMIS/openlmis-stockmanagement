dbb4bf0f73c53300dd68ada29da9edc2b845d2bb
Paweł Albecki <palbecki@soldevelo.com>
2017-08-29 15:41:03 +0200

    move repository tests to IT

--- src/integration-test/java/org/openlmis/stockmanagement/repository/StockCardTemplatesRepositoryTest.java
+++ src/integration-test/java/org/openlmis/stockmanagement/repository/StockCardTemplatesRepositoryTest.java
@@ -15,6 +15,10 @@
 
 package org.openlmis.stockmanagement.repository;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.openlmis.stockmanagement.testutils.StockCardTemplateBuilder.createTemplate;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openlmis.stockmanagement.domain.template.AvailableStockCardFields;
@@ -23,10 +27,6 @@ import org.openlmis.stockmanagement.domain.template.StockCardTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.openlmis.stockmanagement.testutils.StockCardTemplateBuilder.createTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,7 +37,7 @@ public class StockCardTemplatesRepositoryTest {
 
   @Test
   public void should_search_for_stock_card_template_by_facility_type_and_program()
-          throws Exception {
+      throws Exception {
     //given
     StockCardTemplate template = createTemplate();
 
