4b2492892b98eeb5efe5a4199f17e1a5656f4821
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-11-15 11:49:40 +0100

    OLMIS-3533: Added name for indexes

--- src/main/resources/db/migration/20171115093556845__create_unique_indexes_on_stock_cards.sql
+++ src/main/resources/db/migration/20171115093556845__create_unique_indexes_on_stock_cards.sql
@@ -1,3 +1,8 @@
 -- https://stackoverflow.com/a/8289253
-CREATE UNIQUE INDEX ON stock_cards(facilityId, programId, orderableId, lotId) WHERE lotId IS NOT NULL;
-CREATE UNIQUE INDEX ON stock_cards(facilityId, programId, orderableId) WHERE lotId IS NULL;
+CREATE UNIQUE INDEX uniq_stock_card_facility_program_product_lot
+ON stock_cards(facilityId, programId, orderableId, lotId)
+WHERE lotId IS NOT NULL;
+
+CREATE UNIQUE INDEX uniq_stock_card_facility_program_product
+ON stock_cards(facilityId, programId, orderableId)
+WHERE lotId IS NULL;
