5d71de195ef1fe871de37e69fd855eebdeb3ceeb
Chongsun Ahn <cahn922@yahoo.com>
2018-01-08 13:26:34 -0800

    OLMIS-3463 Update ERD generation file
    For new dockerized version of schema spy.

--- generateErdFor.sh
+++ generateErdFor.sh
@@ -6,20 +6,22 @@ docker pull openlmis/$1
 sudo mkdir -p /var/www/html/erd-$1
 sudo chown -R $USER:$USER /var/www/html/erd-$1
 
-wget http://nchc.dl.sourceforge.net/project/schemaspy/schemaspy/SchemaSpy%205.0.0/schemaSpy_5.0.0.jar -O schemaSpy_5.0.0.jar
-wget http://central.maven.org/maven2/org/postgresql/postgresql/9.4-1201-jdbc41/postgresql-9.4-1201-jdbc41.jar -O postgresql-9.4-1201.jdbc41.jar
-
 wget https://raw.githubusercontent.com/OpenLMIS/openlmis-config/master/.env -O .env \
 && wget https://raw.githubusercontent.com/OpenLMIS/openlmis-$1/master/docker-compose.erd-generation.yml -O docker-compose.erd-generation.yml \
 && (/usr/local/bin/docker-compose -f docker-compose.erd-generation.yml up &) \
 && sleep 90 \
 && sudo rm /var/www/html/erd-$1/* -rf \
-&& (sudo java -jar schemaSpy_5.0.0.jar -t pgsql -host localhost:$2 -db open_lmis -u postgres -p $DBPASSWORD -dp postgresql-9.4-1201.jdbc41.jar -all -schemaSpec '^(?!pg_catalog$|information_schema$).*' -hq -o /var/www/html/erd-$1 &) \
+&& mkdir output \
+&& chmod 777 output \
+&& (docker run --rm --network openlmis$1erdgeneration_default -v $WORKSPACE/output:/output schemaspy/schemaspy:snapshot -t pgsql -host db -port 5432 -db open_lmis -s $1 -u postgres -p $DBPASSWORD -I "(data_loaded)|(schema_version)|(jv_.*)" -norows -hq &) \
 && sleep 30 \
 && /usr/local/bin/docker-compose -f docker-compose.erd-generation.yml down --volumes \
+&& sudo chown -R $USER:$USER output \
+&& mv output/* /var/www/html/erd-$1 \
 && rm erd-$1.zip -f \
 && pushd /var/www/html/erd-$1 \
-&& zip -r $WORKSPACE/erd-$1.zip * \
+&& zip -r $WORKSPACE/erd-$1.zip . \
 && popd \
+&& rmdir output \
 && rm .env \
 && rm docker-compose.erd-generation.yml
