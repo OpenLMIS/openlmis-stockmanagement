a60f645a5f549166bf53bcc184aaf87201b7a145
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2018-05-25 11:04:35 +0200

    OLMIS-4717: Added migration with consumed and received tags. Also updated demo data.

---
+++ src/main/resources/db/migration/20180525090330398__add_consumed_and_received_tags.sql
@@ -0,0 +1,5 @@
+INSERT INTO stockmanagement.stock_card_line_item_reason_tags (
+    tag, reasonId
+) VALUES
+    ('received', '313f2f5f-0c22-4626-8c49-3554ef763de3'),
+    ('consumed', 'b5c27da7-bdda-4790-925a-9484c5dfb594');
