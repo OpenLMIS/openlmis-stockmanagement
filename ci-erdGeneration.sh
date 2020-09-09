3c27b84041260af709de8e9558fd84dcef30ee37
Chongsun Ahn <cahn922@yahoo.com>
2020-09-08 17:52:27 -0700

    Fix build script for ERD generation
    * Use 6.1.0 version of SchemaSpy to resolve errors
    * Extend wait to ensure SchemaSpy finishes before tearing down

--- ci-erdGeneration.sh
+++ ci-erdGeneration.sh
@@ -29,8 +29,8 @@ wget https://raw.githubusercontent.com/OpenLMIS/openlmis-ref-distro/master/setti
 && mkdir output \
 && chmod 777 output \
 && export COMPOSE_PROJECT_NAME_LOWER_CASE=`echo "$COMPOSE_PROJECT_NAME" | tr '[:upper:]' '[:lower:]'` \
-&& (docker run --rm --network ${COMPOSE_PROJECT_NAME_LOWER_CASE//.}_default -v $WORKSPACE/erd/output:/output schemaspy/schemaspy:snapshot -t pgsql -host db -port 5432 -db open_lmis -s stockmanagement -u postgres -p p@ssw0rd -I "(data_loaded)|(schema_version)|(jv_.*)" -norows -hq &) \
-&& sleep 30 \
+&& (docker run --rm --network ${COMPOSE_PROJECT_NAME_LOWER_CASE//.}_default -v $WORKSPACE/erd/output:/output schemaspy/schemaspy:6.1.0 -t pgsql -host db -port 5432 -db open_lmis -s stockmanagement -u postgres -p p@ssw0rd -I "(data_loaded)|(schema_version)|(jv_.*)" -norows -hq &) \
+&& sleep 90 \
 && /usr/local/bin/docker-compose down --volumes \
 && sudo chown -R $USER:$USER output \
 && mv output/* /var/www/html/erd-stockmanagement \
