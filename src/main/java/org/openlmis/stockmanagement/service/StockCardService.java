6fde47adaf1c68e5cdba8c1d26b59080d9d87dde
Krzysiek Pasiek <kpasiek@soldevelo.com>
2021-11-02 13:23:16 +0100

    SELV3-371 Make setInactive return Optional

--- src/main/java/org/openlmis/stockmanagement/service/StockCardService.java
+++ src/main/java/org/openlmis/stockmanagement/service/StockCardService.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
