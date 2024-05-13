c8a28f245193f43dc32d65215e4037552e24291a
Szymon Radziszewski <sradziszewski@soldevelo.com>
2024-05-13 14:06:29 +0200

    OAM-65: Fixed imports and line length

--- src/main/java/org/openlmis/stockmanagement/web/ValidSourceDestinationSearchParams.java
+++ src/main/java/org/openlmis/stockmanagement/web/ValidSourceDestinationSearchParams.java
@@ -15,16 +15,17 @@
 
 package org.openlmis.stockmanagement.web;
 
+import static java.util.Arrays.asList;
+import static org.openlmis.stockmanagement.i18n.MessageKeys.ERROR_INVALID_PARAMS;
+import static org.openlmis.stockmanagement.i18n.MessageKeys.ERROR_PROVIDED_FACILITY_ID_WITHOUT_PROGRAM_ID;
+import static org.openlmis.stockmanagement.i18n.MessageKeys.ERROR_PROVIDED_PROGRAM_ID_WITHOUT_FACILITY_ID;
+
+import java.util.Collections;
+import java.util.UUID;
 import org.openlmis.stockmanagement.exception.ValidationMessageException;
 import org.openlmis.stockmanagement.util.Message;
 import org.openlmis.stockmanagement.util.UuidUtil;
 import org.springframework.util.MultiValueMap;
-
-import java.util.Collections;
-import java.util.UUID;
-
-import static java.util.Arrays.asList;
-import static org.openlmis.stockmanagement.i18n.MessageKeys.*;
 
 public class ValidSourceDestinationSearchParams {
 
@@ -95,8 +96,8 @@ public class ValidSourceDestinationSearchParams {
   }
 
   private void validate() {
-    if (!Collections.unmodifiableList(asList(PROGRAM_ID, FACILITY_ID, GEOGRAPHIC_ZONE_ID, INCLUDE_DISABLED))
-        .containsAll(queryParams.keySet())) {
+    if (!Collections.unmodifiableList(asList(PROGRAM_ID, FACILITY_ID, GEOGRAPHIC_ZONE_ID,
+            INCLUDE_DISABLED)).containsAll(queryParams.keySet())) {
       throw new ValidationMessageException(new Message(ERROR_INVALID_PARAMS));
     }
     if (queryParams.keySet().contains(PROGRAM_ID) && !queryParams.keySet().contains(FACILITY_ID)) {
