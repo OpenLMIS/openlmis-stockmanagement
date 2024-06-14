0a9b827af5225e42b148484853cda3aa5d2856eb
tsznaj <tsznajderski@soldevelo.com>
2024-06-14 16:05:12 +0200

    OAM-213: Added scripts to update unit of orderable id to sample one for stock card, stock event line items and physical inventory line items.

---
+++ src/main/resources/db/migration/20240614133422000__update_stock_event_line_items_with_sample_unit_of_orderable.sql
@@ -0,0 +1,2 @@
+UPDATE stock_event_line_items
+    SET unitoforderableid = 'c86e7d33-f8f8-4e0d-b540-89b16ffd71f2' WHERE unitoforderableid IS NULL;
---
+++ src/main/resources/db/migration/20240614133438513__update_physical_inventory_line_items_with_sample_unit_of_orderable.sql
@@ -0,0 +1,2 @@
+UPDATE physical_inventory_line_items
+    SET unitoforderableid = 'c86e7d33-f8f8-4e0d-b540-89b16ffd71f2' WHERE unitoforderableid IS NULL;
