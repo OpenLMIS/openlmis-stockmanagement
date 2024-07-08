f00e4aa371352860d6b28905a0bcbbc1896ecac7
Szymon Radziszewski <117299684+sradziszewski@users.noreply.github.com>
2024-07-08 11:04:21 +0200

    OIS-14: Added configuration for Transifex CLI usage (#62)
    Changed --resource value
    Updated the version of docker-dev used to v9

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Sync with Transifex
 /transifex/sync_transifex.sh \
-  --resource openlmis-stockmanagement.messages \
+  --resource openlmis-stockmanagement \
   --pattern 'src/main/resources/messages_<lang>.properties' \
   --source-file src/main/resources/messages_en.properties
 
