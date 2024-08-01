9ad227db21e6d1aacff5923ba02dcd5b897b72d4
Piotr Wargulak <pwargulak@soldevelo.com>
2024-08-01 10:27:39 +0200

    Use TomcatConnectorCustomizer to enable TCP keepalive

--- src/main/java/org/openlmis/stockmanagement/Application.java
+++ src/main/java/org/openlmis/stockmanagement/Application.java
@@ -27,6 +27,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -107,5 +109,17 @@ public class Application {
   @Bean
   public Callback flywayCallback() {
     return new ExportSchemaFlywayCallback();
+  }
+
+  /**
+   * Customize default Tomcat connector with socket.soKeepAlive
+   * (https://tomcat.apache.org/tomcat-9.0-doc/config/http.html) attribute.
+   *
+   * @return WebServerFactoryCustomizer
+   */
+  @Bean
+  public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer() {
+    return factory -> factory
+        .addConnectorCustomizers(connector -> connector.setAttribute("socket.soKeepAlive", true));
   }
 }
