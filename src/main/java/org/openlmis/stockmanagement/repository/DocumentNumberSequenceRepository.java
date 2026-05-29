9750c460351fc0afd8cead06c8934e03b3666968
lwojewnik <lwojewnik@soldevelo.com>
2026-05-29 15:20:43 +0200

    [SELV3-839] jenkins fix

--- src/main/java/org/openlmis/stockmanagement/repository/DocumentNumberSequenceRepository.java
+++ src/main/java/org/openlmis/stockmanagement/repository/DocumentNumberSequenceRepository.java
@@ -10,7 +10,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details. You should have received a copy of
  * the GNU Affero General Public License along with this program. If not, see
- * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
  */
 
 package org.openlmis.stockmanagement.repository;
